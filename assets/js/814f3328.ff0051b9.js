c9139be2606da38916740346529c7420279e98ef
thomashannestad <thomas@barman-hanssen.no>
2025-08-21 06:26:01 +0000

    Deploy website - based on b1d921c51bf810bfca9f7b30b3095134933e2eeb

--- assets/js/814f3328.ff0051b9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkiskole=self.webpackChunkiskole||[]).push([[2535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Nye frav\xe6rsregler fra august 2025","permalink":"/blog/frav\xe6r"},{"title":"Omlegging av timeplanverkt\xf8yet","permalink":"/blog/timeplan"},{"title":"Dokumentasjon p\xe5 nett","permalink":"/blog/hola"}]}')}}]);
